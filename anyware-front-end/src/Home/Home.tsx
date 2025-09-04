a0b7e048d675b62de0fda917f79c3f9904a7992e
hussienfci <hussienfci@gmail.com>
2025-09-04 12:00:08 +0300

    add nextjs for front-end

---
+++ anyware-front-end/src/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      
+
+        <div className="grid grid-cols-2 mx-auto my-auto">
+            <a href="/Dashboard">login</a>
+            <a href="/Home">Stay</a>  
+        </div>
+  
+    </div>
+  )
+}
+
+export default Home
---
+++ anyware-front-end/src/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+      welcome btch
+    </div>
+  )
+}
+
+export default Dashboard
